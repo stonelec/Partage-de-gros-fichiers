f6e054cedd78da62ea3bdcfe06f478a1a06361bc
Elouaaaan <elouan.pennec@gmail.com>
2025-04-02 15:16:45 -0400

    route file

---
+++ routes/file.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/upload", function (req, res, next) {});
+router.get("/download", function (req, res, next) {});
+
+module.exports = router;
