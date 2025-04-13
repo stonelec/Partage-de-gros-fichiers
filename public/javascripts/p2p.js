deadc0a1ae244b6178a5116d2075eb9e920e7c2e
vince <3millestone@gmail.com>
2025-04-13 19:52:25 -0400

    remove stun edit

--- public/javascripts/p2p.js
+++ public/javascripts/p2p.js
@@ -5,11 +5,6 @@ async function uploadFile(input) {
             rtcConfig: {
                 iceServers: [
                     { urls: 'stun:stun.l.google.com:19302' }, // STUN
-                    {
-                        urls: 'turn:relay1.openwebrtc.io:443',
-                        username: 'openwebrtc',
-                        credential: 'webrtc'
-                    } // TURN public
                 ]
             }
         }
